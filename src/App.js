1f265f587e86ee4e6d51c4caf81fefff6a3e89bb
Sulav <sulavshrestha023@gmail.com>
2022-06-25 10:44:43 +0545

    new components and router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Error from "./Error";
+import Help from "./Help";
+import Home from "./Home";
 import Navbar from "./Navbar";
 
 const App = () => {
   return (
     <Router>
       <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </Router>
   );
 };
