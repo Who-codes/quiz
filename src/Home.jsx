a5d2a682c42513c7504d742123c6b08c61117b62
Sulav <sulavshrestha023@gmail.com>
2022-06-26 12:49:58 +0545

    response returned

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,7 +6,29 @@ const Home = () => {
   const [quiz, setQuiz] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  return <div>Home</div>;
+  const getData = async () => {
+    const res = await fetch("https://opentdb.com/api.php?amount=10");
+    const data = await res.json();
+    setQuiz(data);
+    console.log(quiz);
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    getData();
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <main>
+      {quiz.results.map((item, index) => (
+        <Quiz {...item} key={index} />
+      ))}
+    </main>
+  );
 };
 
 export default Home;
--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Quiz = () => {
+const Quiz = ({ question, correct_answer, incorrect_answers }) => {
   return <div>Quiz</div>;
 };
 
