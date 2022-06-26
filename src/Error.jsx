78013d84b1a96590086f3abe07cbfc95db52fd74
Sulav <sulavshrestha023@gmail.com>
2022-06-26 11:35:48 +0545

    error page

--- src/Error.jsx
+++ src/Error.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className="error">
+      <h2>Nothing Here !</h2>
+      <Link to={"/"}>
+        <button className="btn primary-btn">back home</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Error;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Loading from "./Loading";
+import Quiz from "./Quiz";
 
 const Home = () => {
+  const [quiz, setQuiz] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   return <div>Home</div>;
 };
 
